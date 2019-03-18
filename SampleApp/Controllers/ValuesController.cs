71db1915f21230c5425dd1ee7aebb75986a17ad4
Stuart Lang <stuart.b.lang@gmail.com>
2019-03-18 09:19:05 +0000

    Add sample app

---
+++ SampleApp/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NSwag.PetStore.Client;
+
+namespace SampleApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IPetStoreClient petStoreClient;
+
+        public ValuesController(IPetStoreClient petStoreClient)
+        {
+            this.petStoreClient = petStoreClient;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get(CancellationToken ct)
+        {
+            var stu = await petStoreClient.GetUserByNameAsync("Stu", ct);
+            return new[] { stu.Email };
+        }
+    }
+}
